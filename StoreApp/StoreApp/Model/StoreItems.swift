326b8533ff3240e1908806f2e726ca0bb7689bf6
harrison.tom <harrison.tom@kakaocorp.com>
2021-02-01 16:26:39 +0900

    json 파싱 및 데이터 속성 저장, url String 타입에서 enum으로 변경

--- StoreApp/StoreApp/Model/StoreItems.swift
+++ StoreApp/StoreApp/Model/StoreItems.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+enum JsonFileName : String {
+    case best = "best"
+    case mask = "mask"
+    case grocery = "grocery"
+    case fryingpan = "fryingpan"
+    
+    static let jsonFileName = [best, mask, grocery, fryingpan]
+}
+
+class StoreItems {
+    var allItems : [[Item]] = [[]]
+    
+    var count : Int {
+        return allItems.count
+    }
+    
+    subscript(index : Int) -> [Item] {
+        return allItems[index]
+    }
+    
+    init() {
+        for iter in JsonFileName.jsonFileName {
+            guard let url = URL(string: "http://public.codesquad.kr/jk/kakao-2021/"+iter.rawValue+".json") else { return }
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    do {
+                        if let decodeData = try? JSONDecoder().decode([Item].self, from : data){
+                            self.allItems.append(decodeData)
+                        }
+                    } catch {}
+                }
+            }.resume()
+        }
+    }
+    
+    private func decode(_ data : Data) -> [Item]? {
+        do {
+            return try JSONDecoder().decode([Item].self, from : data)
+        } catch {
+            return nil
+        }
+    }
+}
+
+
