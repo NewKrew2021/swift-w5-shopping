afb6d73a44b95570de1cbf46c7132ee7a9ca2ad6
blue.1 <blue.1@kakaocorp.com>
2021-02-01 12:50:55 +0900

    first commit

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by 이청원 on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
