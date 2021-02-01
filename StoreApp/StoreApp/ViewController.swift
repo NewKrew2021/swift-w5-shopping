b94b35af62bfca6e1476ba650f08edc072abd220
bean-milky <bean.milky@kakaocorp.com>
2021-02-01 10:34:00 +0900

    [ADD] Empty Project

---
+++ StoreApp/StoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreApp
+//
+//  Created by bean Milky on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
