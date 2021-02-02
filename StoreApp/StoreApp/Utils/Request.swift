d23bef37d0b58418f42f4267f9e68030e82ad19f
harrison.tom <harrison.tom@kakaocorp.com>
2021-02-02 22:54:28 +0900

    줄 정렬 및 Toast 메세지 추가

--- StoreApp/StoreApp/Utils/Request.swift
+++ StoreApp/StoreApp/Utils/Request.swift
@@ -7,24 +7,27 @@
 //
 
 import Foundation
+import Toaster
 
 class Request {
     static func requestHttp(){
         for iter in JsonFileName.jsonFileName {
-                    guard let url = URL(string: "http://public.codesquad.kr/jk/kakao-2021/"+iter.rawValue+".json") else { return }
-                    URLSession.shared.dataTask(with: url) { (data, response, error) in
-                        guard let data = data else { return }
-                        DispatchQueue.main.async {
-                            do {
-                                if let decodeData = try? JSONDecoder().decode([Item].self, from : data){
-                                    let userInfo: [AnyHashable: Any] = [iter.rawValue:decodeData]
-                                    NotificationCenter.default.post(name: NSNotification.Name("saveItem"), object: self, userInfo: userInfo)
-                                }
-                            } catch {
-        //                        print(error)
-                            }
+            guard let url = URL(string: "http://public.codesquad.kr/jk/kakao-2021/"+iter.rawValue+".json") else { return }
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    do {
+                        if let decodeData = try? JSONDecoder().decode([Item].self, from : data){
+                            let userInfo: [AnyHashable: Any] = [iter.rawValue:decodeData]
+                            NotificationCenter.default.post(name: NSNotification.Name("saveItem"), object: self, userInfo: userInfo)
                         }
-                    }.resume()
+                    } catch {
+                        let toast = Toast(text: iter.rawValue + "json 을 읽어올 수 없습니다.")
+                        ToastView.appearance().font = UIFont.systemFont(ofSize: 13, weight: .bold)
+                        toast.show()
+                    }
                 }
+            }.resume()
+        }
     }
 }
