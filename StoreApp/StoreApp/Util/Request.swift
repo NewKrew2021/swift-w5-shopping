a10003642eaf42bcfd37d371586a811cf5e92695
harrison.tom <harrison.tom@kakaocorp.com>
2021-02-02 11:00:03 +0900

    네트워크 객체 분리

--- StoreApp/StoreApp/Util/Request.swift
+++ StoreApp/StoreApp/Util/Request.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Request {
+    static func requestHttp(){
+        for iter in JsonFileName.jsonFileName {
+                    guard let url = URL(string: "http://public.codesquad.kr/jk/kakao-2021/"+iter.rawValue+".json") else { return }
+                    URLSession.shared.dataTask(with: url) { (data, response, error) in
+                        guard let data = data else { return }
+                        DispatchQueue.main.async {
+                            do {
+                                if let decodeData = try? JSONDecoder().decode([Item].self, from : data){
+                                    let userInfo: [AnyHashable: Any] = [iter.rawValue:decodeData]
+                                    NotificationCenter.default.post(name: NSNotification.Name("saveItem"), object: self, userInfo: userInfo)
+                                }
+                            } catch {
+        //                        print(error)
+                            }
+                        }
+                    }.resume()
+                }
+    }
+}
