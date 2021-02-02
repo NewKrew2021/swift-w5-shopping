444c8b21ce5226aaaa0ebb67b810899f99e0b4cd
harrison.tom <harrison.tom@kakaocorp.com>
2021-02-02 22:52:47 +0900

    cache 관리 클래스

---
+++ StoreApp/StoreApp/Utils/CacheStorage.swift
@@ -0,0 +1,27 @@
+//
+//  CacheStorage.swift
+//  StoreApp
+//
+//  Created by 이준형 on 2021/02/02.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import Foundation
+
+class CacheStorage {
+    static let cacheUrl = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+
+    static func retrieve(_ fileName: String) -> Data? {
+        guard FileManager.default.fileExists(atPath: cacheUrl.path) else { return nil }
+        let fileUrl = cacheUrl.appendingPathComponent(fileName)
+        return FileManager.default.contents(atPath: fileUrl.path)
+    }
+
+    static func save(_ fileName: String, _ data: Data?) throws {
+        let fileUrl = cacheUrl.appendingPathComponent(fileName)
+        if FileManager.default.fileExists(atPath: fileUrl.path) {
+            try FileManager.default.removeItem(at: fileUrl)
+        }
+        FileManager.default.createFile(atPath: fileUrl.path, contents: data, attributes: nil)
+    }
+}
---
+++ StoreApp/StoreApp/Utils/Downloader.swift
@@ -0,0 +1,9 @@
+//
+//  Downloader.swift
+//  StoreApp
+//
+//  Created by 이준형 on 2021/02/02.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import Foundation
