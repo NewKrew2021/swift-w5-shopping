f56deb38e30bd8fa6382759e95543d4171011570
harrison.tom <harrison.tom@kakaocorp.com>
2021-02-04 11:22:14 +0900

    데이터 속성 추가

---
+++ StoreApp/StoreApp/View/myCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  myCollectionView.swift
+//  StoreApp
+//
+//  Created by 이준형 on 2021/02/04.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import UIKit
+
+class myCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
