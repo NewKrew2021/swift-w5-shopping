2c52cab2d89a013d141ba7db4d57a36b43fc6112
kor-Randy <randy_95@naver.com>
2021-02-04 16:47:44 +0900

    add: DetailVC UI Components

--- StoreApp/StoreApp/Detail/DetailViewController.swift
+++ StoreApp/StoreApp/Detail/DetailViewController.swift
@@ -8,18 +8,54 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    // MARK: Internal
+
     let viewModel = DetailViewModel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         addScrollView()
+        addReviewView()
+        addTitleView()
         scrollView.addImages(urls: viewModel.detail?.data.previewUrls ?? [])
     }
-    
+
+    // MARK: Private
+
     private var scrollView = DetailScrollView()
     
-    private func addScrollView(){
+    private var starLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .blue
+        label.text = "★ 3.5"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.sizeToFit()
+        return label
+    }()
+    
+    private var reviewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .blue
+        label.text = "리뷰 000건"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.sizeToFit()
+        return label
+    }()
+    
+    private var titleTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.systemFont(ofSize: 20)
+        textView.textContainer.maximumNumberOfLines = 2
+        textView.text = "[모든 부자재 100% 국내산] 4중구조 KF94 황사마스크 성인용 100매"
+        textView.textColor = .black
+        return textView
+    }()
+
+    private func addScrollView() {
         view.addSubview(scrollView)
         scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
         scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
@@ -27,4 +63,20 @@ class DetailViewController: UIViewController {
         scrollView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.4).isActive = true
     }
     
+    private func addReviewView() {
+        view.addSubview(starLabel)
+        view.addSubview(reviewLabel)
+        starLabel.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 5).isActive = true
+        reviewLabel.topAnchor.constraint(equalTo: starLabel.topAnchor).isActive = true
+        starLabel.trailingAnchor.constraint(equalTo: reviewLabel.leadingAnchor, constant: -20).isActive = true
+        reviewLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 30).isActive = true
+    }
+    
+    private func addTitleView(){
+        view.addSubview(titleTextView)
+        titleTextView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8).isActive = true
+        titleTextView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.08).isActive = true
+        titleTextView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        titleTextView.topAnchor.constraint(equalTo: starLabel.bottomAnchor).isActive = true
+    }
 }
