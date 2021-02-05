e77c8a6dd569c482fccffb2b432c8b61ba96d9b5
eden.joon <eden.joon@kakaocorp.com>
2021-02-05 17:58:27 +0900

    스토리보드로 뷰 재구성

--- StoreApp/StoreApp/Controller/DetailViewController.swift
+++ StoreApp/StoreApp/Controller/DetailViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import WebKit
 
 class DetailViewController: UIViewController {
-    let mainScrollView = UIScrollView()
-    let imageScrollView = UIScrollView()
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    @IBOutlet weak var imageScrollView: UIScrollView!
     var imageViews: [UIImageView] = []
-    var imageStackView = UIStackView()
-    var webView: WKWebView!
+    @IBOutlet weak var webView: WKWebView!
     var productDetail: ProductDetail?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,33 +34,8 @@ class DetailViewController: UIViewController {
     }
 
     func initView() {
-        view.addSubview(mainScrollView)
-        mainScrollView.translatesAutoresizingMaskIntoConstraints = false
-        mainScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        mainScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        mainScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        mainScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-
-        mainScrollView.addSubview(imageScrollView)
-        imageScrollView.translatesAutoresizingMaskIntoConstraints = false
-        imageScrollView.widthAnchor.constraint(equalTo: mainScrollView.frameLayoutGuide.widthAnchor).isActive = true
-        imageScrollView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
-        imageScrollView.topAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.topAnchor).isActive = true
-        imageScrollView.leadingAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.leadingAnchor).isActive = true
-
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        mainScrollView.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
-//        webView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        webView.widthAnchor.constraint(equalTo: mainScrollView.frameLayoutGuide.widthAnchor).isActive = true
-        webView.topAnchor.constraint(equalTo: imageScrollView.bottomAnchor, constant: 20).isActive = true
-        webView.bottomAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.bottomAnchor).isActive = true
-        webView.leadingAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.leadingAnchor).isActive = true
-        webView.scrollView.isScrollEnabled = false
         webView.navigationDelegate = self
-
-        mainScrollView.contentSize = CGSize(width: view.bounds.width, height: imageScrollView.bounds.height + webView.bounds.height + 20)
     }
 
     func setImageAtImageScroll() {
@@ -87,8 +61,6 @@ class DetailViewController: UIViewController {
 
         guard let descriptionHtml = productDetail?.description else { return }
         webView.loadHTMLString(meta_java + descriptionHtml, baseURL: nil)
-        webView.centerXAnchor.constraint(equalTo: mainScrollView.contentLayoutGuide.centerXAnchor).isActive = true
-        webView.widthAnchor.constraint(equalTo: mainScrollView.frameLayoutGuide.widthAnchor).isActive = true
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -101,7 +73,6 @@ extension DetailViewController: WKNavigationDelegate{
     func webView(_ webView: WKWebView, didFinish _: WKNavigation!) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             self.webView.heightAnchor.constraint(equalToConstant: webView.scrollView.contentSize.height).isActive = true
-            self.mainScrollView.contentSize = CGSize(width: self.view.bounds.width, height: self.imageScrollView.bounds.height + self.webView.bounds.height + 20)
         }
     }
 }
